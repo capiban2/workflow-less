a58f160da0665496aba156d67b8b92fb6f1fc862
capiban2 <randybenq@gmail.com>
2023-10-01 14:09:52 +0300

    some middleware added

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const exp = require('express');
 const app = exp();
 const port = 3000;
 
-app.get('/', () =>{
+const requestTime = (req,res,next)  =>{
+	req.requestTime = Date.now();
+	next();
+}
+
+app.get('/', requestTime, (req,res) =>{
 	console.log('get request');
 	console.log(`NEW ROW ADDED`);
 	console.log(`another row added`);
+	console.log(`Request time : ${req.requestTime}.`)
 	res.send('hello world');
 });
 
